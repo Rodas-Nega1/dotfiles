c62b85e7dd8dd4e3a5068b8c8f65976431cbfdeb
Rodas-Nega1 <rodas.nega@mths.ca>
2022-09-22 01:57:09 +0000

    im tired

---
+++ .config/nvim/lua/user/options.lua
@@ -0,0 +1,11 @@
+vim.opt.encoding = "utf-8"
+
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.numberwidth = 4
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartident = true
+
+vim.opt.scrolloff = 6
+vim.opt.wrap = false
