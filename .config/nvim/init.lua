0b1b6fe30ae1f5dacb09b46173fa218cfde09644
Rodas-Nega1 <rodas.nega@mths.ca>
2022-10-24 13:26:05 +0000

    what

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,8 @@
 require "user.options"
+require "user.plugins"
+
+require('feline').setup()
+require "user.colorscheme"
+vim.cmd "colorscheme onedarker"
+require "user.cmp" 
+
--- .config/nvim/lua/user/options.lua
+++ .config/nvim/lua/user/options.lua
@@ -1,11 +1,12 @@
 vim.opt.encoding = "utf-8"
 
+vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.numberwidth = 4
 vim.opt.tabstop = 2
 vim.opt.expandtab = true
-vim.opt.smartident = true
+vim.opt.smartindent = true
 
 vim.opt.scrolloff = 6
 vim.opt.wrap = false
